d716eb50190d9c8a80bb63f4f2369f245e94f5b5
Dirk Kelly <github@dirkkelly.com>
2016-02-27 17:58:40 +0900

    WIP: 5 years of snowboarding

--- config.rb
+++ config.rb
@@ -10,6 +10,7 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 page 'essays/*', layout: 'essay'
+page 'blog/*', layout: 'post'
 
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
